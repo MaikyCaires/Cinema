0b5c93d36b6b5076b297d301755c585235e696a1
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2024-02-23 16:41:00 -0300

    tudo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Cinema/', // Adicione esta linha com o caminho do seu repositório
+  base: '/Jogos/', // Adicione esta linha com o caminho do seu repositório
   plugins: [react()],
 });
